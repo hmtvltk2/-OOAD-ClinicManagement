9ba57477290c6e4ac0ba7ca24377e59dece765fb
HMTri <huynhminhtri2412@gmail.com>
2017-01-04 01:21:15 +0700

    Commit some thing

---
+++ ClinicManager.DataModel/ReportPayment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicManager.DataModel
+{
+    public class ReportPayment
+    {
+        public string ServiceName { get; set; }
+        public decimal ServiceFee { get; set; }
+    }
+}
